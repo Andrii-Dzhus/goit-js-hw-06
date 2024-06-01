22be285b5bb79367a57ef0b3208a977f422b7354
Andrii Dzhus <158450996+Andrii-Dzhus@users.noreply.github.com>
2024-06-01 20:47:17 +0300

    01/06/24

--- js/task-2.js
+++ js/task-2.js
@@ -1,31 +1,30 @@
 class Storage {
-  #items;
+    #items;
 
-  constructor(store){
-    this.#items = store;
-  }
+    constructor(store) {
+        this.#items = store;
+    }
 
-  getItems(){
-    return this.#items;
-  }
+    getItems() {
+        return this.#items;
+    }
 
-  addItem(newItem){
-    this.#items.push(newItem);
-
-  }
-  removeItem(itemToRemove){
-    this.#items = this.#items.filter(item => item !== itemToRemove);
+    addItem(newItem) {
+        this.#items.push(newItem);
+    }
+    removeItem(itemToRemove) {
+        this.#items = this.#items.filter((item) => item !== itemToRemove);
+    }
 }
-}
 
-const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+const storage = new Storage(['Nanitoids', 'Prolonger', 'Antigravitator']);
 console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
 
-storage.addItem("Droid");
+storage.addItem('Droid');
 console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
 
-storage.removeItem("Prolonger");
+storage.removeItem('Prolonger');
 console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
 
-storage.removeItem("Scaner");
+storage.removeItem('Scaner');
 console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
